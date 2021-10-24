edcc7bf12ddc2fa4da1b5d41b71d8695def89bcd
job karani <job.nyambura@student.moringaschool.com>
2021-10-24 14:22:54 +0300

    creates a set method to run before each test cases.

--- user-test.py
+++ user-test.py
@@ -5,4 +5,12 @@ class TestUser(unittest.TestClass):
     """
     Test class that defines test case for the user class behaviour.
     """
-    
+
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_user = User("Job","ayub09877") #create user object
+
+        if __name__='__main__':
+            unittest.main()
--- user.py
+++ user.py
@@ -4,18 +4,16 @@ class User:
     """
     user_list=[]
 
-    def __init__(self,full_name,username,password):
+    def __init__(self,username,password):
 
         '''
         __init__ method that helps us define properties for our objects.
 
         Args:
-            full_name: New user full name.
             username: New user username.
             password : New user password.
         '''
 
-        self.full_name=full_name
         self.username=username
         self.password=password
 
