b1702e808321bdc68efbbd066806b78bbb78f6e2
job karani <job.nyambura@student.moringaschool.com>
2021-10-24 14:28:11 +0300

    creates a test init function to test whether the object is initialized properly

--- user-test.py
+++ user-test.py
@@ -12,5 +12,15 @@ class TestUser(unittest.TestClass):
         """
         self.new_user = User("Job","ayub09877") #create user object
 
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user,"Job")
+        self.assertEqual(self.new_user,"ayub09877")
+
+
+
+
         if __name__='__main__':
             unittest.main()
