04039b232c7f56b3037192f114f5262b3e4f9409
job karani <job.nyambura@student.moringaschool.com>
2021-10-24 16:43:38 +0300

    creates test save user function

--- user-test.py
+++ user-test.py
@@ -1,26 +1,32 @@
 import unittest
 from user import User
 
-class TestUser(unittest.TestClass):
+class TestUser(unittest.TestCase):
     """
     Test class that defines test case for the user class behaviour.
     """
 
     def setUp(self):
-        """
+        '''
         Set up method to run before each test cases.
-        """
+        '''
         self.new_user = User("Job","ayub09877") #create user object
 
     def test_init(self):
-        """
+        '''
         test_init test case to test if the object is initialized properly
-        """
-        self.assertEqual(self.new_user,"Job")
-        self.assertEqual(self.new_user,"ayub09877")
+        '''
+        self.assertEqual(self.new_user.username, "Job")
+        self.assertEqual(self.new_user.password, "ayub09877")
+
+    def test_save_user(self):
+        '''
+        test case that tests if the user is being saved properly
+        '''
+        self.new_user.save_user()
+
+        self.assertEqual(len(User.user_list),1)
 
 
-
-
-        if __name__='__main__':
-            unittest.main()
+if __name__ == '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 class User:
-    """
+    '''
     class that generates new instances of user
-    """
-    user_list=[]
+    '''
+    user_list = []
 
     def __init__(self,username,password):
 
@@ -14,8 +14,15 @@ class User:
             password : New user password.
         '''
 
-        self.username=username
-        self.password=password
+        self.username = username
+        self.password = password
+
+        def save_user(self):
+            '''
+            function to add a new user to the user list
+            '''
+
+            User.user_list.append(self)
 
         
 
