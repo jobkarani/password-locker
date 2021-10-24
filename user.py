79456dbbd7633ec25c54bfcc15a84217013c1191
job karani <job.nyambura@student.moringaschool.com>
2021-10-24 13:59:05 +0300

    creates a function to initialize the object

--- user.py
+++ user.py
@@ -4,5 +4,20 @@ class user:
     """
     user_list=[]
 
+    def __init__(self,full_name,username,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            full_name: New user full name.
+            username: New user username.
+            password : New user password.
+        '''
+
+        self.full_name=full_name
+        self.username=username
+        self.password=password
+
         
 
