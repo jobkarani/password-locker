f266d8d0a42de95937c197191ec5a7eb0eaa7b91
job karani <job.nyambura@student.moringaschool.com>
2021-10-25 08:07:25 +0300

    adds shebang

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 from user import User
 from credentials import Credential
 
+def create_user(username, password, email):
+    '''
+    function to create a new user.
+    Args:
+       username: New user username.
+        password : New user password.
+        email : New user email.
+    '''
+    new_user = User(username, password, email)
+    return new_user
+
+def 
