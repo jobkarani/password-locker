c0145bebaf32aa1d937c328e9ddf53724d1859f7
job karani <job.nyambura@student.moringaschool.com>
2021-10-25 11:04:42 +0300

    adds logic to exit, create,login,display users

--- run.py
+++ run.py
@@ -63,3 +63,71 @@ def display_all_users():
     return User.display_users()
 
 
+def main():
+    print("Hey welcome to PASSWORD LOCKER.What is your name?")
+    username = input()
+    print(f"Hey {username}. what would you like to do)")
+    print("\n")
+
+    while True:
+        print("Use these short codes to choose an option:\n cn - Create an account \n ds - Display available users \n lg - Login to your account \n ex - Exit password locker")
+        print("\n")
+
+        option = input()
+
+        if option == 'cn':
+            print("~" * 15)
+            print("Create an account")
+            print("~" * 15)
+
+            print("Enter your username")
+            username = input()
+            print("*" * 8)
+
+            print("Enter your email")
+            email = input()
+            print("*" * 8)
+
+            print("Enter your password")
+            password = input()
+            print("*" * 8)
+
+            save_user(create_user(username, password, email))
+            print("*" * 8)
+
+            print(f"{username}, your account has been successfully created. You may proceed login.")
+
+        elif option == 'ds':
+            if display_all_users():
+                print("This is the list of users:")
+                print("~" * 15)
+
+                for user in display_all_users():
+                    print(f"Username: {user.username}")
+                    print(f"Email: {user.email}")
+
+            else:
+                print("\n")
+                print("Ooooopps....You dont have any saved users yet!!!")
+                print("\n")
+
+        elif option == 'lg':
+            print("~" * 15)
+            print("Login")
+            print("~" * 15)
+
+            print("Enter your username")
+            username = input()
+            print("*" * 8)
+
+            print("Enter your password")
+            password = input()
+            print("*" * 8)
+
+        else:
+            print("Bye :)")
+            break
+
+
+if __name__ == '__main__':
+    main()
