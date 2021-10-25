e5cb2db926c2ef1d4f9cfd29f5fbf1a84d76f747
job karani <job.nyambura@student.moringaschool.com>
2021-10-25 06:55:02 +0300

    creates a function to initialize credential objects

--- credentials.py
+++ credentials.py
@@ -1,2 +1,8 @@
 class Credential:
-    
+    def __init__(self, social_media, account, password):
+        '''
+        function helps us define properties of our objects
+        '''
+        self.social_media = social_media
+        self.account = account
+        self.password = password 
